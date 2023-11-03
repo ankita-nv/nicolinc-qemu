6db8a9e028755b0698d58ecd9563984e96b023fd
Zhenzhong Duan <zhenzhong.duan@intel.com>
2023-11-03 16:54:01 +0800

    backends/iommufd: Add helpers for allocating user-managed HWPT
    Include helper to allocate user-managed hwpt and helper for cache
    invalidation as user-managed HWPT needs to sync cache per modifications.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>

--- include/sysemu/iommufd.h
+++ include/sysemu/iommufd.h
@@ -50,6 +50,13 @@ int iommufd_backend_unmap_dma(IOMMUFDBackend *be, uint32_t ioas_id,
 bool iommufd_backend_get_device_info(IOMMUFDBackend *be, uint32_t devid,
                                      uint32_t *type, void *data, uint32_t len,
                                      Error **errp);
+int iommufd_backend_alloc_hwpt(IOMMUFDBackend *be, uint32_t dev_id,
+                               uint32_t pt_id, uint32_t flags,
+                               uint32_t data_type, uint32_t data_len,
+                               void *data_ptr, uint32_t *out_hwpt);
+int iommufd_backend_invalidate_cache(IOMMUFDBackend *be, uint32_t hwpt_id,
+                                     uint32_t data_type, uint32_t entry_len,
+                                     uint32_t *entry_num, void *data_ptr);
 
 #define TYPE_HOST_IOMMU_DEVICE_IOMMUFD TYPE_HOST_IOMMU_DEVICE "-iommufd"
 #endif
