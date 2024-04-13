8e184e691846f8a19e788ab62253331b5a47e58e
Nicolin Chen <nicolinc@nvidia.com>
2024-04-13 00:25:40 +0000

    backends/iommufd_device: Introduce iommufd_viommu_get_shared_page
    Allow to mmap a shared kernel page via iommufd.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- include/sysemu/iommufd.h
+++ include/sysemu/iommufd.h
@@ -80,6 +80,10 @@ int iommufd_viommu_invalidate_cache(IOMMUFDBackend *be, uint32_t viommu_id,
 struct IOMMUFDVqueue *iommufd_viommu_alloc_queue(IOMMUFDViommu *viommu,
                                                  uint32_t data_type,
                                                  uint32_t len, void *data_ptr);
+void *iommufd_viommu_get_shared_page(IOMMUFDViommu *viommu,
+                                     uint32_t size, bool readonly);
+void iommufd_viommu_put_shared_page(IOMMUFDViommu *viommu,
+                                    void *page, uint32_t size);
 
 #define TYPE_HOST_IOMMU_DEVICE_IOMMUFD TYPE_HOST_IOMMU_DEVICE "-iommufd"
 OBJECT_DECLARE_TYPE(HostIOMMUDeviceIOMMUFD, HostIOMMUDeviceIOMMUFDClass,
