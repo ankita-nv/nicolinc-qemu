a2379655d9e74cd5e993f26d4787c6c95c91af2e
Nicolin Chen <nicolinc@nvidia.com>
2024-06-19 04:03:37 +0000

    hw/arm/virt: Get the number of host-level SMMUv3 instances
    Nested SMMUv3 feature requires the support/presence of host-level SMMUv3
    instance(s). Add a helper to read the sysfs for the number of instances.
    Log them in a vms list using a new struct VirtNestedSmmu.
    
    This will be used by a following patch to assign a passthrough device to
    corresponding nested SMMUv3 instance.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- include/hw/arm/virt.h
+++ include/hw/arm/virt.h
@@ -137,6 +137,12 @@ struct VirtMachineClass {
     bool no_ns_el2_virt_timer_irq;
 };
 
+typedef struct VirtNestedSmmu {
+    int index;
+    char *smmu_node;
+    QLIST_ENTRY(VirtNestedSmmu) next;
+} VirtNestedSmmu;
+
 struct VirtMachineState {
     MachineState parent;
     Notifier machine_done;
@@ -155,6 +161,7 @@ struct VirtMachineState {
     bool ras;
     bool mte;
     bool dtb_randomness;
+    int num_nested_smmus;
     OnOffAuto acpi;
     VirtGICType gic_version;
     struct IOMMUFDBackend *iommufd;
@@ -180,6 +187,7 @@ struct VirtMachineState {
     char *oem_id;
     char *oem_table_id;
     bool ns_el2_virt_timer_irq;
+    QLIST_HEAD(, VirtNestedSmmu) nested_smmu_list;
 };
 
 #define VIRT_ECAM_ID(high) (high ? VIRT_HIGH_PCIE_ECAM : VIRT_PCIE_ECAM)
